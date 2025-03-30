c13e58fe859138048004ad788d5e6282cce44a8e
MarijaBB <101042225+MarijaBB@users.noreply.github.com>
2025-03-30 20:49:14 +0200

    error handling

--- model/make_connection.py
+++ model/make_connection.py
@@ -1,8 +1,13 @@
 import mysql.connector
 def mydb_connection():
-    return mysql.connector.connect(
-            host="localhost",
-            user="",     # not showing publicly
-            password="", # not showing publicly
-            database = "feelDB"
-        )
+    try:
+        mydb = mysql.connector.connect(
+                host="localhost",
+                user="",     # not showing publicly
+                password="", # not showing publicly
+                database = "feelDB"
+            )
+        return mydb
+    except mysql.connector.Error as err:
+        print("Error ",err)
+        return None
